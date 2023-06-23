81257385a48fed4e49358b81337c500cf3020aee
Emmanuel Gaillot <emmanuel.gaillot@gmail.com>
2023-06-23 17:57:27 +0200

    Extrais génération trame XML dans vue spécifique

--- src/ootsFrance.js
+++ src/ootsFrance.js
@@ -1,5 +1,7 @@
 const express = require('express');
 
+const JustificatifEducation = require('./vues/justificatifEducation');
+
 const creeServeur = (config) => {
   const { adaptateurUUID } = config;
   let serveur;
@@ -7,29 +9,10 @@ const creeServeur = (config) => {
 
   app.get('/response/educationEvidence', (_requete, reponse) => {
     const uuid = adaptateurUUID.genereUUID();
+    const justificatif = new JustificatifEducation(uuid);
 
     reponse.set('Content-Type', 'text/xml');
-    reponse.send(`<?xml version="1.0" encoding="UTF-8"?>
-<query:QueryResponse xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-    xmlns:rs="urn:oasis:names:tc:ebxml-regrep:xsd:rs:4.0"
-    xmlns:rim="urn:oasis:names:tc:ebxml-regrep:xsd:rim:4.0" xmlns:sdg="http://data.europa.eu/p4s"
-    xmlns:query="urn:oasis:names:tc:ebxml-regrep:xsd:query:4.0"
-    xmlns:xlink="http://www.w3.org/1999/xlink"
-    status="urn:oasis:names:tc:ebxml-regrep:ResponseStatusType:Success"
-    requestId="urn:uuid:${uuid}">
-  <rim:Slot name="SpecificationIdentifier">
-  </rim:Slot>
-  <rim:Slot name="EvidenceResponseIdentifier">
-  </rim:Slot>
-  <rim:Slot name="IssueDateTime">
-  </rim:Slot>
-  <rim:Slot name="EvidenceProvider">
-  </rim:Slot>
-  <rim:Slot name="EvidenceRequester">
-  </rim:Slot>
-  <rim:RegistryObjectList>
-  </rim:RegistryObjectList>
-</query:QueryResponse>`);
+    reponse.send(justificatif.enXML());
   });
 
   app.get('/', (_requete, reponse) => {
