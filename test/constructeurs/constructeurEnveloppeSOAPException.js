64e666bfe6d20bfbb2e319b59e156259c7facfef
Emmanuel Gaillot <emmanuel.gaillot@beta.gouv.fr>
2023-10-18 14:20:18 +0200

    [NETTOYAGE] Extrais un constructeur d'enveloppe SOAP pour les tests

---
+++ test/constructeurs/constructeurEnveloppeSOAPException.js
@@ -0,0 +1,131 @@
+class ConstructeurEnveloppeSOAPException {
+  static erreurAutorisationRequise() {
+    return new ConstructeurEnveloppeSOAPException()
+      .avecErreur({
+        type: 'rs:AuthorizationExceptionType',
+        code: 'EDM:ERR:0002',
+        detail: 'The server needs authorisation and preview on its side to process the request',
+        message: 'Missing Authorization',
+        severite: 'urn:sr.oots.tech.ec.europa.eu:codes:ErrorSeverity:EDMErrorResponse:PreviewRequired',
+      });
+  }
+
+  avecErreur({
+    type,
+    code,
+    detail,
+    message,
+    severite,
+  }) {
+    this.typeErreur = type;
+    this.codeErreur = code;
+    this.detailErreur = detail;
+    this.messageErreur = message;
+    this.severiteErreur = severite;
+
+    return this;
+  }
+
+  avecExpediteur(expediteur) {
+    this.expediteur = expediteur;
+    return this;
+  }
+
+  avecIdConversation(id) {
+    this.idConversation = id;
+    return this;
+  }
+
+  avecIdMessage(id) {
+    this.idMessage = id;
+    return this;
+  }
+
+  avecURLRedirection(url) {
+    this.urlRedirection = url;
+    return this;
+  }
+
+  construis() {
+    const message = `<query:QueryResponse
+        xmlns:rs="urn:oasis:names:tc:ebxml-regrep:xsd:rs:4.0"
+        xmlns:query="urn:oasis:names:tc:ebxml-regrep:xsd:query:4.0"
+        xmlns:rim="urn:oasis:names:tc:ebxml-regrep:xsd:rim:4.0"
+        status="urn:oasis:names:tc:ebxml-regrep:ResponseStatusType:Failure"
+        requestId="urn:uuid:11111111-1111-1111-1111-111111111111">
+
+  <rs:Exception xsi:type="${this.typeErreur}"
+                code="${this.codeErreur}"
+                detail="${this.detailErreur}"
+                message="${this.messageErreur}"
+                severity="${this.severiteErreur}">
+    <rim:Slot name="PreviewLocation">
+      <rim:SlotValue xsi:type="rim:StringValueType">
+        <rim:Value>${this.urlRedirection}</rim:Value>
+      </rim:SlotValue>
+    </rim:Slot>
+    <rim:Slot name="Timestamp">
+      <rim:SlotValue xsi:type="rim:DateTimeValueType">
+        <rim:Value>2023-10-02T11:30:00.000+02:00</rim:Value>
+      </rim:SlotValue>
+    </rim:Slot>
+  </rs:Exception>
+</query:QueryResponse>`;
+
+    const messageBase64 = Buffer.from(message).toString('base64');
+
+    return `
+<soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope">
+  <soap:Header>
+    <ns5:Messaging>
+      <ns5:UserMessage>
+        <ns5:MessageInfo>
+            <ns5:Timestamp>2023-09-24T09:07:45.000Z</ns5:Timestamp>
+            <ns5:MessageId>${this.idMessage}</ns5:MessageId>
+        </ns5:MessageInfo>
+        <ns5:PartyInfo>
+            <ns5:From>
+                <ns5:PartyId type="urn:cef.eu:names:identifier:EAS:0204">${this.expediteur}</ns5:PartyId>
+                <ns5:Role>http://sdg.europa.eu/edelivery/gateway</ns5:Role>
+            </ns5:From>
+            <ns5:To>
+                <ns5:PartyId type="urn:oasis:names:tc:ebcore:partyid-type:unregistered">GovernmentNLGateway01</ns5:PartyId>
+                <ns5:Role>http://sdg.europa.eu/edelivery/gateway</ns5:Role>
+            </ns5:To>
+        </ns5:PartyInfo>
+        <ns5:CollaborationInfo>
+            <ns5:Service type="urn:oasis:names:tc:ebcore:ebrs:ebms:binding:1.0">QueryManager</ns5:Service>
+            <ns5:Action>ExceptionResponse</ns5:Action>
+            <ns5:ConversationId>${this.idConversation}</ns5:ConversationId>
+        </ns5:CollaborationInfo>
+        <ns5:MessageProperties>
+            <ns5:Property name="originalSender" type="urn:cef.eu:names:identifier:EAS:0204">02-SchoolAuthority-34</ns5:Property>
+            <ns5:Property name="finalRecipient" type="urn:cef.eu:names:identifier:EAS:0190">NL222332239</ns5:Property>
+        </ns5:MessageProperties>
+        <ns5:PayloadInfo>
+            <ns5:PartInfo href="cid:530ad1e2-5eaf-4a9a-8192-227432eea95d@it.nrw">
+                <ns5:PartProperties>
+                    <ns5:Property name="MimeType">application/x-ebrs+xml</ns5:Property>
+                    <ns5:Property name="CompressionType">application/gzip</ns5:Property>
+                </ns5:PartProperties>
+            </ns5:PartInfo>
+        </ns5:PayloadInfo>
+      </ns5:UserMessage>
+    </ns5:Messaging>
+  </soap:Header>
+  <soap:Body>
+    <ns4:retrieveMessageResponse
+      xmlns:xmime="http://www.w3.org/2005/05/xmlmime"
+      xmlns:ns5="http://docs.oasis-open.org/ebxml-msg/ebms/v3.0/ns/core/200704/"
+      xmlns:ns4="http://eu.domibus.wsplugin/">
+      <payload payloadId="cid:11111111-1111-1111-1111-111111111111@oots.eu">
+        <value>${messageBase64}</value>
+      </payload>
+    </ns4:retrieveMessageResponse>
+  </soap:Body>
+</soap:Envelope>
+    `;
+  }
+}
+
+module.exports = ConstructeurEnveloppeSOAPException;
--- test/domibus/reponseRecuperationMessage.spec.js
+++ test/domibus/reponseRecuperationMessage.spec.js
@@ -1,154 +1,64 @@
+const ConstructeurEnveloppeSOAPException = require('../constructeurs/constructeurEnveloppeSOAPException');
 const ReponseRecuperationMessage = require('../../src/domibus/reponseRecuperationMessage');
 const { ErreurReponseRequete } = require('../../src/erreurs');
 
 describe('La réponse à une requête Domibus de récupération de message', () => {
   it("connaît l'URL de redirection spécifiée", () => {
-    const message = `<query:QueryResponse
-        xmlns:rs="urn:oasis:names:tc:ebxml-regrep:xsd:rs:4.0"
-        xmlns:query="urn:oasis:names:tc:ebxml-regrep:xsd:query:4.0"
-        xmlns:rim="urn:oasis:names:tc:ebxml-regrep:xsd:rim:4.0"
-        status="urn:oasis:names:tc:ebxml-regrep:ResponseStatusType:Failure"
-        requestId="urn:uuid:11111111-1111-1111-1111-111111111111">
-
-  <rs:Exception xsi:type="rs:AuthorizationExceptionType"
-                code="EDM:ERR:0002"
-                detail="The server needs authorisation and preview on its side to process the request"
-                message="Missing Authorization"
-                severity="urn:sr.oots.tech.ec.europa.eu:codes:ErrorSeverity:EDMErrorResponse:PreviewRequired">
-    <rim:Slot name="PreviewLocation">
-      <rim:SlotValue xsi:type="rim:StringValueType">
-        <rim:Value>https://example.com/preview/12345678-1234-1234-1234-1234567890ab</rim:Value>
-      </rim:SlotValue>
-    </rim:Slot>
-    <rim:Slot name="Timestamp">
-      <rim:SlotValue xsi:type="rim:DateTimeValueType">
-        <rim:Value>2023-10-02T11:30:00.000+02:00</rim:Value>
-      </rim:SlotValue>
-    </rim:Slot>
-  </rs:Exception>
-</query:QueryResponse>`;
-
-    const messageBase64 = Buffer.from(message).toString('base64');
-
-    const reponse = new ReponseRecuperationMessage(`
-<soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope">
-  <soap:Header>
-    <ns5:Messaging>
-      <ns5:UserMessage>
-        <!-- Entêtes SOAP. Inutiles pour ce test -->
-      </ns5:UserMessage>
-    </ns5:Messaging>
-  </soap:Header>
-  <soap:Body>
-    <ns4:retrieveMessageResponse
-      xmlns:xmime="http://www.w3.org/2005/05/xmlmime"
-      xmlns:ns5="http://docs.oasis-open.org/ebxml-msg/ebms/v3.0/ns/core/200704/"
-      xmlns:ns4="http://eu.domibus.wsplugin/">
-      <payload payloadId="cid:11111111-1111-1111-1111-111111111111@oots.eu">
-        <value>${messageBase64}</value>
-      </payload>
-    </ns4:retrieveMessageResponse>
-  </soap:Body>
-</soap:Envelope>
-    `);
+    const enveloppeSOAP = ConstructeurEnveloppeSOAPException.erreurAutorisationRequise()
+      .avecURLRedirection('https://example.com/preview/12345678-1234-1234-1234-1234567890ab')
+      .construis();
+    const reponse = new ReponseRecuperationMessage(enveloppeSOAP);
 
     expect(reponse.urlRedirection()).toEqual('https://example.com/preview/12345678-1234-1234-1234-1234567890ab');
   });
 
-  describe("à partir de l'entête", () => {
-    const reponse = new ReponseRecuperationMessage(`
-<soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope">
-  <soap:Header>
-    <ns5:Messaging xmlns:xmime="http://www.w3.org/2005/05/xmlmime"
-                   xmlns:ns5="http://docs.oasis-open.org/ebxml-msg/ebms/v3.0/ns/core/200704/"
-                   xmlns:ns4="http://eu.domibus.wsplugin/" mustUnderstand="false">
-      <ns5:UserMessage mpc="http://docs.oasis-open.org/ebxml-msg/ebms/v3.0/ns/core/200704/defaultMPC">
-        <ns5:MessageInfo>
-            <ns5:Timestamp><!-- … --></ns5:Timestamp>
-            <ns5:MessageId>11111111-1111-1111-1111-111111111111@oots.eu</ns5:MessageId>
-        </ns5:MessageInfo>
+  it("connaît le type d'action liée au message", () => {
+    const enveloppeSOAP = ConstructeurEnveloppeSOAPException.erreurAutorisationRequise()
+      .construis();
+    const reponse = new ReponseRecuperationMessage(enveloppeSOAP);
 
-        <ns5:PartyInfo>
-            <ns5:From>
-                <ns5:PartyId type="urn:oasis:names:tc:ebcore:partyid-type:unregistered:oots-simulator">AP_SI_01</ns5:PartyId>
-                <ns5:Role>http://sdg.europa.eu/edelivery/gateway</ns5:Role>
-            </ns5:From>
-            <ns5:To><!-- … --></ns5:To>
-        </ns5:PartyInfo>
-
-        <ns5:CollaborationInfo>
-          <ns5:Service type="urn:oasis:names:tc:ebcore:ebrs:ebms:binding:1.0">QueryManager</ns5:Service>
-          <ns5:Action>ExecuteQueryRequest</ns5:Action>
-          <ns5:ConversationId>12345678-1234-1234-1234-1234567890ab</ns5:ConversationId>
-        </ns5:CollaborationInfo>
-        <!-- d'autres informations inutiles pour le test -->
-      </ns5:UserMessage>
-    </ns5:Messaging>
-  </soap:Header>
-  <soap:Body>
-    <-- Corps du message -->
-  </soap:Body>
-</soap:Envelope>
-    `);
-
-    it("connaît le type d'action liée au message", () => {
-      expect(reponse.action()).toEqual('ExecuteQueryRequest');
-    });
-
-    it("connaît l'identifiant de la conversation", () => {
-      expect(reponse.idConversation()).toEqual('12345678-1234-1234-1234-1234567890ab');
-    });
-
-    it("connaît l'expéditeur", () => {
-      expect(reponse.expediteur()).toEqual('AP_SI_01');
-    });
-
-    it('connaît son identifiant de message', () => {
-      expect(reponse.idMessage()).toEqual('11111111-1111-1111-1111-111111111111@oots.eu');
-    });
+    expect(reponse.action()).toEqual('ExceptionResponse');
   });
 
-  describe("dans le cas d'une réponse en erreur", () => {
-    const message = `<query:QueryResponse
-        xmlns:rs="urn:oasis:names:tc:ebxml-regrep:xsd:rs:4.0"
-        xmlns:query="urn:oasis:names:tc:ebxml-regrep:xsd:query:4.0"
-        xmlns:rim="urn:oasis:names:tc:ebxml-regrep:xsd:rim:4.0"
-        status="urn:oasis:names:tc:ebxml-regrep:ResponseStatusType:Failure"
-        requestId="urn:uuid:11111111-1111-1111-1111-111111111111">
+  it("connaît l'identifiant de la conversation", () => {
+    const enveloppeSOAP = ConstructeurEnveloppeSOAPException.erreurAutorisationRequise()
+      .avecIdConversation('12345678-1234-1234-1234-1234567890ab')
+      .construis();
+    const reponse = new ReponseRecuperationMessage(enveloppeSOAP);
 
-  <rs:Exception xsi:type="rs:ObjectNotFoundExceptionType"
-                code="EDM:ERR:0004"
-                message="Object not found"
-                severity="urn:oasis:names:tc:ebxml-regrep:ErrorSeverityType:Error">
-  </rs:Exception>
-  <!-- … -->
-</query:QueryResponse>`;
+    expect(reponse.idConversation()).toEqual('12345678-1234-1234-1234-1234567890ab');
+  });
 
-    const messageBase64 = Buffer.from(message).toString('base64');
+  it("connaît l'expéditeur", () => {
+    const enveloppeSOAP = ConstructeurEnveloppeSOAPException.erreurAutorisationRequise()
+      .avecExpediteur('AP_SI_01')
+      .construis();
+    const reponse = new ReponseRecuperationMessage(enveloppeSOAP);
 
-    const reponse = new ReponseRecuperationMessage(`
-<soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope">
-  <soap:Header>
-    <ns5:Messaging>
-      <ns5:UserMessage>
-        <!-- Entêtes SOAP. Inutiles pour ce test -->
-      </ns5:UserMessage>
-    </ns5:Messaging>
-  </soap:Header>
-  <soap:Body>
-    <ns4:retrieveMessageResponse
-      xmlns:xmime="http://www.w3.org/2005/05/xmlmime"
-      xmlns:ns5="http://docs.oasis-open.org/ebxml-msg/ebms/v3.0/ns/core/200704/"
-      xmlns:ns4="http://eu.domibus.wsplugin/">
-      <payload payloadId="cid:11111111-1111-1111-1111-111111111111@oots.eu">
-        <value>${messageBase64}</value>
-      </payload>
-    </ns4:retrieveMessageResponse>
-  </soap:Body>
-</soap:Envelope>
-    `);
+    expect(reponse.expediteur()).toEqual('AP_SI_01');
+  });
 
+  it('connaît son identifiant de message', () => {
+    const enveloppeSOAP = ConstructeurEnveloppeSOAPException.erreurAutorisationRequise()
+      .avecIdMessage('11111111-1111-1111-1111-111111111111@oots.eu')
+      .construis();
+    const reponse = new ReponseRecuperationMessage(enveloppeSOAP);
+
+    expect(reponse.idMessage()).toEqual('11111111-1111-1111-1111-111111111111@oots.eu');
+  });
+
+  describe("dans le cas d'une réponse en erreur pièce inexistante", () => {
     it("lève une erreur à la tentative de lecture de l'URL de redirection", (suite) => {
+      const enveloppeSOAP = new ConstructeurEnveloppeSOAPException()
+        .avecErreur({
+          type: 'rs:ObjectNotFoundExceptionType',
+          code: 'EDM:ERR:0004',
+          message: 'Object not found',
+          severite: 'urn:oasis:names:tc:ebxml-regrep:ErrorSeverityType:Error',
+        })
+        .construis();
+      const reponse = new ReponseRecuperationMessage(enveloppeSOAP);
+
       try {
         reponse.urlRedirection();
         suite('Une `ErreurReponseRequete` aurait dû être levée.');
