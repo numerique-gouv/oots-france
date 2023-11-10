75ae3ff532a6b86c19d7ee83b683239afd742beb
Emmanuel Gaillot <emmanuel.gaillot@beta.gouv.fr>
2023-11-10 14:26:29 +0100

    Retrouve le payload par son type MIME

--- src/domibus/enteteMessageRecu.js
+++ src/domibus/enteteMessageRecu.js
@@ -1,6 +1,7 @@
 class EnteteMessageRecu {
   constructor(donneesEntete) {
     this.enteteMessageUtilisateur = donneesEntete.Messaging.UserMessage;
+    this.infosPayloads = [].concat(this.enteteMessageUtilisateur.PayloadInfo.PartInfo);
   }
 
   action() {
@@ -19,16 +20,24 @@ class EnteteMessageRecu {
     return this.enteteMessageUtilisateur.MessageInfo.MessageId;
   }
 
-  idPayload() {
-    const infos = [].concat(this.enteteMessageUtilisateur.PayloadInfo.PartInfo);
-
-    const infosPayloadMessageEBMS = infos.find((i) => {
+  idPayload(typeMime) {
+    const infosPayloadMessageEBMS = this.infosPayloads.find((i) => {
       const proprietes = [].concat(i.PartProperties.Property);
       const proprieteMimeType = proprietes.find((p) => p['@_name'] === 'MimeType');
-      return proprieteMimeType['#text'] === 'application/x-ebrs+xml';
+      return proprieteMimeType['#text'] === typeMime;
     });
 
     return infosPayloadMessageEBMS['@_href'];
+  }
+
+  payloads() {
+    return this.infosPayloads.reduce((acc, infosPayload) => {
+      const proprietes = [].concat(infosPayload.PartProperties.Property);
+      const proprieteTypeMime = proprietes.find((p) => p['@_name'] === 'MimeType');
+      const typeMime = proprieteTypeMime['#text'];
+
+      return Object.assign(acc, { [typeMime]: infosPayload['@_href'] });
+    }, {});
   }
 }
 
