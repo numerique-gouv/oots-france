8517bb547e8ffd17ae05c9abf863d4d1bd6aa326
Fabien Lamarque <fabien.lamarque@beta.gouv.fr>
2024-01-16 16:13:37 +0100

    Ajoute deux tests unitaires sur la partie pièce jointe dans les en-têtes.

--- test/ebms/enteteReponse.spec.js
+++ test/ebms/enteteReponse.spec.js
@@ -1,8 +1,36 @@
 const Entete = require('../../src/ebms/entete');
 const EnteteReponse = require('../../src/ebms/enteteReponse');
+const PointAcces = require('../../src/ebms/pointAcces');
 
 describe("L'entête d'une réponse", () => {
+  const adaptateurUUID = {};
+  const horodateur = {};
+  const config = { adaptateurUUID, horodateur };
+  const destinataire = new PointAcces('unID', 'unTypeID');
+
+  beforeEach(() => {
+    adaptateurUUID.genereUUID = () => '';
+    horodateur.maintenant = () => '';
+  });
+
   it('connaît son action', () => {
     expect(EnteteReponse.action()).toEqual(Entete.EXECUTION_REPONSE);
+  });
+
+  it("ne contient pas de pièce jointe si aucune n'est fournie", () => {
+    const enteteReponse = new EnteteReponse(config, { destinataire });
+
+    expect(enteteReponse.enXML()).not.toContain('application/pdf');
+  });
+
+  it('peut contenir une pièce jointe', () => {
+    const entete = new EnteteReponse(config, {
+      idPayload: 'unIdPayload',
+      idPieceJointe: 'unIdentifiantPieceJointe',
+      destinataire,
+    });
+
+    expect(entete.enXML()).toContain('application/pdf');
+    expect(entete.enXML()).toContain('PartInfo href="unIdentifiantPieceJointe"');
   });
 });
