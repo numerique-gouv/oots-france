c83fff63379b3d99523552ceced491ce76ef6b09
Emmanuel Gaillot <emmanuel.gaillot@beta.gouv.fr>
2024-02-09 16:07:14 +0100

    [NETTOYAGE] déplace déclaration route

--- src/ootsFrance.js
+++ src/ootsFrance.js
@@ -31,6 +31,25 @@ const creeServeur = (config) => {
     reponse.send({ etatEcouteur: ecouteurDomibus.etat() });
   });
 
+  app.get('/auth/cles_publiques', (_requete, reponse) => {
+    const { kty, n, e } = adaptateurEnvironnement.clePriveeJWK();
+    const idClePublique = adaptateurChiffrement.cleHachage(n);
+
+    const clePubliqueDansJWKSet = {
+      keys: [{
+        kid: idClePublique,
+        use: 'enc',
+        kty,
+        e,
+        n,
+      }],
+    };
+
+    reponse.set('Content-Type', 'application/json');
+    reponse.status(200)
+      .send(clePubliqueDansJWKSet);
+  });
+
   app.get('/auth/fcplus/connexion', (requete, reponse) => {
     const { code, state } = requete.query;
     if (typeof state === 'undefined' || state === '') {
@@ -106,25 +125,6 @@ const creeServeur = (config) => {
 
   app.get('/', (_requete, reponse) => {
     reponse.status(501).send('Not Implemented Yet!');
-  });
-
-  app.get('/auth/cles_publiques', (_requete, reponse) => {
-    const { kty, n, e } = adaptateurEnvironnement.clePriveeJWK();
-    const idClePublique = adaptateurChiffrement.cleHachage(n);
-
-    const clePubliqueDansJWKSet = {
-      keys: [{
-        kid: idClePublique,
-        use: 'enc',
-        kty,
-        e,
-        n,
-      }],
-    };
-
-    reponse.set('Content-Type', 'application/json');
-    reponse.status(200)
-      .send(clePubliqueDansJWKSet);
   });
 
   const arreteEcoute = (suite) => serveur.close(suite);
