dcd309885aa40eb45f50f27c36ba7181ae234ed5
Emmanuel Gaillot <emmanuel.gaillot@beta.gouv.fr>
2024-02-26 12:04:28 +0100

    Extrais routes `/`

---
+++ test/routes/routesBase.spec.js
@@ -0,0 +1,40 @@
+const axios = require('axios');
+
+const serveurTest = require('./serveurTest');
+const { leveErreur } = require('./utils');
+
+describe('Le serveur des routes `/`', () => {
+  const serveur = serveurTest();
+  let port;
+
+  beforeEach((suite) => serveur.initialise(() => {
+    port = serveur.port();
+    suite();
+  }));
+
+  afterEach((suite) => serveur.arrete(suite));
+
+  describe('sur GET /', () => {
+    it("affiche qu'il n'y a pas pas d'utilisateur courant par défaut", () => (
+      axios.get(`http://localhost:${port}/`)
+        .then((reponse) => {
+          expect(reponse.status).toBe(200);
+          expect(reponse.data).toContain("Pas d'utilisateur courant");
+        })
+        .catch(leveErreur)));
+
+    it("affiche prénom et nom de l'utilisateur courant s'il existe", () => {
+      serveur.adaptateurChiffrement().verifieJeton = () => Promise.resolve({
+        given_name: 'Sandra',
+        family_name: 'Nicouette',
+      });
+
+      return axios.get(`http://localhost:${port}/`)
+        .then((reponse) => {
+          expect(reponse.status).toBe(200);
+          expect(reponse.data).toContain('Utilisateur courant : Sandra Nicouette');
+        })
+        .catch(leveErreur);
+    });
+  });
+});
