e4e8a812108bd485bddd95fefd8c9a310dffb119
Emmanuel Gaillot <emmanuel.gaillot@beta.gouv.fr>
2024-05-17 14:58:44 +0200

    [NETTOYAGE] Déplace responsabilité génération XML dans objet métier

---
+++ test/ebms/typeJustificatif.spec.js
@@ -0,0 +1,26 @@
+const TypeJustificatif = require('../../src/ebms/typeJustificatif');
+
+describe('Le type de justificatif', () => {
+  it("s'affiche en XML", () => {
+    const typeJustificatif = new TypeJustificatif({
+      id: 'unIdentifiant',
+      descriptions: { EN: 'someType' },
+      formatDistribution: TypeJustificatif.FORMAT_PDF,
+    });
+
+    expect(typeJustificatif.enXML()).toBe(`
+<rim:Slot name="EvidenceRequest">
+  <rim:SlotValue xsi:type="rim:AnyValueType">
+    <sdg:DataServiceEvidenceType xmlns="urn:oasis:names:tc:ebxml-regrep:xsd:rim:4.0">
+      <sdg:Identifier>00000000-0000-0000-0000-000000000000</sdg:Identifier>
+      <sdg:EvidenceTypeClassification>unIdentifiant</sdg:EvidenceTypeClassification>
+      <sdg:Title lang="EN">someType</sdg:Title>
+      <sdg:DistributedAs>
+        <sdg:Format>application/pdf</sdg:Format>
+      </sdg:DistributedAs>
+    </sdg:DataServiceEvidenceType>
+  </rim:SlotValue>
+</rim:Slot>
+    `);
+  });
+});
