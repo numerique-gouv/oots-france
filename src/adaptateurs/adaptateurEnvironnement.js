cf85c831d66892c952d7f45c36a5dc1b8e7ba5db
Emmanuel Gaillot <emmanuel.gaillot@beta.gouv.fr>
2024-08-09 18:33:10 +0200

    Initialise dépôt Services Communs par environnement

--- src/adaptateurs/adaptateurEnvironnement.js
+++ src/adaptateurs/adaptateurEnvironnement.js
@@ -1,5 +1,10 @@
 const avecRequetePieceJustificative = () => process.env.AVEC_REQUETE_PIECE_JUSTIFICATIVE === 'true';
 
+const donneesDepotServicesCommunsLocal = () => (
+  JSON.parse(process.env.DONNEES_DEPOT_SERVICES_COMMUNS_LOCAL)
+);
+
 module.exports = {
   avecRequetePieceJustificative,
+  donneesDepotServicesCommunsLocal,
 };
