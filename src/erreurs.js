d75c60c68ae8da7a92cf23544b132b199eb14d7c
Emmanuel Gaillot <emmanuel.gaillot@beta.gouv.fr>
2024-09-12 17:22:19 +0200

    Factorise erreurs EBMS pour simplifier leur traitement
    Co-authored-by: Fabien Lamarque <fabien.lamarque@beta.gouv.fr>

--- src/erreurs.js
+++ src/erreurs.js
@@ -1,14 +1,16 @@
 class ErreurAbsenceReponseDestinataire extends Error {}
 class ErreurAucunMessageDomibusRecu extends Error {}
-class ErreurCodeDemarcheIntrouvable extends Error {}
-class ErreurCodePaysIntrouvable extends Error {}
-class ErreurDestinataireInexistant extends Error {}
 class ErreurEchecAuthentification extends Error {}
 class ErreurInstructionSOAPInconnue extends Error {}
 class ErreurJetonInvalide extends Error {}
 class ErreurReponseRequete extends Error {}
-class ErreurRequeteurInexistant extends Error {}
-class ErreurTypeJustificatifIntrouvable extends Error {}
+
+class ErreurEBMS extends Error {}
+class ErreurCodeDemarcheIntrouvable extends ErreurEBMS {}
+class ErreurCodePaysIntrouvable extends ErreurEBMS {}
+class ErreurDestinataireInexistant extends ErreurEBMS {}
+class ErreurRequeteurInexistant extends ErreurEBMS {}
+class ErreurTypeJustificatifIntrouvable extends ErreurEBMS {}
 
 module.exports = {
   ErreurAbsenceReponseDestinataire,
@@ -17,6 +19,7 @@ module.exports = {
   ErreurCodePaysIntrouvable,
   ErreurDestinataireInexistant,
   ErreurEchecAuthentification,
+  ErreurEBMS,
   ErreurInstructionSOAPInconnue,
   ErreurJetonInvalide,
   ErreurReponseRequete,
