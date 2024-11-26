5e9cb9c4d878e66753be048e642e8d154f618640
Emmanuel Gaillot <emmanuel.gaillot@beta.gouv.fr>
2024-11-26 14:25:49 +0100

    Affiche l'identifiant eIDAS dans le XML s'il existe

--- src/ebms/personnePhysique.js
+++ src/ebms/personnePhysique.js
@@ -1,16 +1,22 @@
 class PersonnePhysique {
   constructor(donnees = {}) {
+    this.identifiantEidas = donnees.identifiantEidas;
     this.nom = donnees.nom;
     this.prenom = donnees.prenom;
     this.dateNaissance = donnees.dateNaissance;
   }
 
   enXML() {
+    const identifiantEidasEnXML = typeof this.identifiantEidas !== 'undefined'
+      ? `<sdg:Identifier schemeID="eidas">${this.identifiantEidas}</sdg:Identifier>`
+      : '';
+
     return `
 <rim:Slot name="NaturalPerson">
   <rim:SlotValue xsi:type="rim:AnyValueType">
     <sdg:Person>
       <sdg:LevelOfAssurance>High</sdg:LevelOfAssurance>
+      ${identifiantEidasEnXML}
       <sdg:FamilyName>${this.nom}</sdg:FamilyName>
       <sdg:GivenName>${this.prenom}</sdg:GivenName>
       <sdg:DateOfBirth>${this.dateNaissance}</sdg:DateOfBirth>
--- test/ebms/personnePhysique.spec.js
+++ test/ebms/personnePhysique.spec.js
@@ -1,9 +1,11 @@
 const PersonnePhysique = require('../../src/ebms/personnePhysique');
+const { parseXML, valeurSlot } = require('../../src/ebms/utils');
 
 describe('Une personne physique', () => {
   it("doit s'afficher en XML", () => {
     const jonas = new PersonnePhysique(
       {
+        identifiantEidas: 'DK/DE/123123123',
         nom: 'Garcia',
         prenom: 'Jose',
         dateNaissance: '1985-12-20',
@@ -15,6 +17,7 @@ describe('Une personne physique', () => {
   <rim:SlotValue xsi:type="rim:AnyValueType">
     <sdg:Person>
       <sdg:LevelOfAssurance>High</sdg:LevelOfAssurance>
+      <sdg:Identifier schemeID="eidas">DK/DE/123123123</sdg:Identifier>
       <sdg:FamilyName>Garcia</sdg:FamilyName>
       <sdg:GivenName>Jose</sdg:GivenName>
       <sdg:DateOfBirth>1985-12-20</sdg:DateOfBirth>
@@ -22,5 +25,13 @@ describe('Une personne physique', () => {
   </rim:SlotValue>
 </rim:Slot>
     `);
+  });
+
+  it("n'affiche pas la balise identifiant s'il n'y a pas d'identifiant renseigné", () => {
+    const personneSansIdentifiantEidas = new PersonnePhysique();
+    const xml = parseXML(personneSansIdentifiantEidas.enXML());
+    const identifiantEidas = valeurSlot('NaturalPerson', xml).Person.Identifier;
+
+    expect(identifiantEidas).toBeUndefined();
   });
 });
